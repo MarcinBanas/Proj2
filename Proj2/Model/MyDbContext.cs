eb55919498b96577e14c9cbfc427b7f76c6527c1
MarcinBanas <mb056625@student.ath.edu.pl>
2022-09-07 15:31:42 +0200

    commit

--- Proj2/Model/MyDbContext.cs
+++ Proj2/Model/MyDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -23,8 +24,7 @@ namespace Proj2.Model
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-9K2SBT4;Initial Catalog=Proj1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["ProjBaza"].ConnectionString);
             }
         }
 
